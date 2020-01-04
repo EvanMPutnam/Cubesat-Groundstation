c00af6be5c84b92e7f830369a95689f8caa258ea
EvanPutnam <emput1998@gmail.com>
2020-01-03 23:55:40 -0500

    Updating kerbal space program demo program

--- kerbal_example/kerbal.py
+++ kerbal_example/kerbal.py
@@ -12,11 +12,17 @@ import api
 DATABASE_PROJECT = "KERBAL_Test"
 DATABASE_PARAMETERS = {
     "Altitude": "Altitude",
-    "Speed": "Speed",
     "Mach": "Mach",
     "Angle_Of_Attack": "Angle_Of_Attack",
     "Latitude": "Latitude",
     "Longitude": "Longitude",
+
+    "Apoapsis": "Apoapsis",
+    "Periapsis": "Periapsis",
+    "Semi_Major_Axis": "Semi_Major_Axis",
+    "Semi_Minor_Axis": "Semi_Minor_Axis",
+    "Period": "Period",
+    "Eccentricity": "Eccentricity"
 }
 
 class Flight_Debug:
@@ -28,6 +34,16 @@ class Flight_Debug:
         self.latitude = random.random()
         self.longitude = random.random()
 
+class Orbit_Debug:
+    def __init__(self):
+        self.apoapsis = random.random()
+        self.periapsis = random.random()
+        self.semi_major_axis = random.random()
+        self.semi_minor_axis = random.random()
+        self.period = random.random()
+        self.eccentricity = random.random()
+        
+
 class Vessel_Debug:
     def __init__(self):
         pass
@@ -35,32 +51,55 @@ class Vessel_Debug:
     def flight(self):
         return Flight_Debug()
 
+    def orbit(self):
+        return Orbit_Debug()
+
 #This is used to update the data display with each respective parameter.
 def update_parameters(vessel, database_project = DATABASE_PROJECT, database_params = DATABASE_PARAMETERS, debug=False):
     #Get flight data
     flight = vessel.flight()
+    orbit = vessel.orbit
     #Get all the datapoints
     altitude = flight.mean_altitude
-    speed = flight.speed
     mach = flight.mach
     aoa = flight.angle_of_attack
     latitude = flight.latitude
     longitude = flight.longitude
-    #Update viewer
+
+    #Get orbit data
+    apoapsis = orbit.apoapsis
+    periapsis = orbit.periapsis
+    semi_major_axis = orbit.semi_major_axis
+    semi_minor_axis = orbit.semi_minor_axis
+    period = orbit.period
+    eccentricity = orbit.eccentricity
+
     if debug:
         print("Starting update")
+
+    #Spacecraft data
     api.update_data(database_params['Altitude'], database_project, altitude, type_of_op=api.REPLACE_OP, debug=debug)
-    api.update_data(database_params['Speed'], database_project, speed, type_of_op=api.REPLACE_OP, debug=debug)
-    api.update_data(database_params['Mach'], database_project, mach, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Mach'], database_project, mach, debug=debug)
     api.update_data(database_params['Angle_Of_Attack'], database_project, aoa, type_of_op=api.REPLACE_OP, debug=debug)
     api.update_data(database_params['Latitude'], database_project, latitude, type_of_op=api.REPLACE_OP, debug=debug)
     api.update_data(database_params['Longitude'], database_project, longitude, type_of_op=api.REPLACE_OP, debug=debug)
+
+    #Orbital elements
+    api.update_data(database_params['Apoapsis'], database_project, apoapsis, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Periapsis'], database_project, periapsis, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Semi_Major_Axis'], database_project, semi_major_axis, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Semi_Minor_Axis'], database_project, semi_minor_axis, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Period'], database_project, period, type_of_op=api.REPLACE_OP, debug=debug)
+    api.update_data(database_params['Eccentricity'], database_project, eccentricity, type_of_op=api.REPLACE_OP, debug=debug)
+
+
     if debug:
         print("Ending update")
 
 #Create connection to krpc
 def create_connection(name_val):
-    conn = krpc.connect(name = name_val)
+    conn = krpc.connect()
+    print("Connection acquired")
     vessel = conn.space_center.active_vessel
     return conn, vessel
 
@@ -79,4 +118,4 @@ def run(debug=False):
         
 
 if __name__ == "__main__":
-    run(debug=True)
+    run(debug=False)
